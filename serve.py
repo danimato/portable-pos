b76e611e5f871bb1594bee92f26f26a4c61030a6
gamer0kayf1n3 <81130943+gamer0kayf1n3@users.noreply.github.com>
2025-12-26 22:28:47 +0800

    threaded mode because of sw.js blocking single-threaded server

--- serve.py
+++ serve.py
@@ -3,14 +3,16 @@ import socketserver
 
 class MyHandler(http.server.SimpleHTTPRequestHandler):
     def end_headers(self):
-        # Force correct MIME type for sw.js
         if self.path.endswith(".js"):
             self.send_header("Content-Type", "application/javascript")
-        # Disable caching to prevent future 304 errors during development
         self.send_header("Cache-Control", "no-store, no-cache, must-revalidate")
         super().end_headers()
 
 PORT = 81
-with socketserver.TCPServer(("", PORT), MyHandler) as httpd:
+
+class ThreadedTCPServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
+    allow_reuse_address = True
+
+with ThreadedTCPServer(("", PORT), MyHandler) as httpd:
     print(f"Serving at http://localhost:{PORT}")
     httpd.serve_forever()
